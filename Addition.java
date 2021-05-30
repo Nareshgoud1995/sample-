5c4f43b0e86bf254260083400f3e9e9b1114ba70
sadhanavemula@gmail.com <sadhanavemula@gmail.com>
2021-05-30 19:21:06 +0530

    anil sample

---
+++ Addition.java
@@ -0,0 +1,13 @@
+package anilencapulation;
+
+public class Addition {
+	private static void sum(int a,int b) {
+		System.out.println("sum is "+(a+b));
+		
+	}
+	public static void main(String[] args) {
+		 Addition add=new  Addition();
+		 add.sum(24, 20);
+	}
+
+}
